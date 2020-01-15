c865eeff8381f7ac9115b70614e89e6b29eb1121
feta <42926543+drppsy@users.noreply.github.com>
2020-01-15 23:15:50 +0800

    settings commit

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -138,11 +138,3 @@ CKEDITOR_CONFIGS= {
 
 #自定义每页的博客数量
 NUMBER_OF_BLOG_IN_EACH_PAGE = 3
-
-#缓存设置
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'my_cache_table',
-    }
-}
