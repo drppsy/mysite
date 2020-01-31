6f191eca2962de0d98d1f400ddef10523bcc0332
feta <42926543+drppsy@users.noreply.github.com>
2020-01-31 14:52:33 +0800

    设置databasePassword

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -28,13 +28,13 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 pymysql.install_as_MySQLdb()
-# DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
         'NAME': 'mysite',  # 数据库名，先前创建的
         'USER': 'root',     # 用户名，可以自己创建用户
-        'PASSWORD': '212233',  # 密码
+        'PASSWORD': DATABASE_PASSWORD,  # 密码
         'HOST': '122.51.4.96',  # mysql服务所在的主机ip
         'PORT': '3306',         # mysql服务端口
     }
